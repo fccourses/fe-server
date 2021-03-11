ac4802ea639dce3f46b67e601596e88eb28bf6e2
Vladimir <freshcode.yt@gmail.com>
2021-03-11 20:46:32 +0200

    Add node server

--- index.js
+++ index.js
@@ -1,22 +1,55 @@
+const http = require('http')
+const fs = require('fs').promises
 
-/* 
+const PORT = 3000
 
-  RESOLVING -> LOADING -> WRAPPING -> EVALUATION -> CACHING
+const users = []
 
-  I
-    1.1 Core modules
-    1.2 node_modules
+const routerGET = {
+  '/': async (req, res) => {
+    const data = await fs.readFile('./views/index.html', 'utf8')
+    return res.end(data)
+  },
+  '/contacts.html': async (req, res) => {
+    const data = await fs.readFile('./views/contacts.html', 'utf8')
+    return res.end(data)
+  },
+  '/about.html': async (req, res) => {
+    const data = await fs.readFile('./views/about.html', 'utf8')
+    return res.end(data)
+  }
+}
 
-  II
-    2.1 filename js | json
-    2.2 directory
-      2.2.1 package.json -> "main":"./entrypoint.js"
-      2.2.2 index js | json
+const requestListener = async (req, res) => {
+  const { method, url } = req
 
-  III
-    throw new Error()
+  if (method === 'GET' && routerGET[url]) {
+    return routerGET[url](req, res)
+  }
 
-*/
+  if (method === 'POST') {
+    if (url === '/create-user') {
+      let jsonString = ''
+      req.on('data', chunk => {
+        jsonString += chunk
+      })
 
-const external = require('./abrakadabra')
-console.log(external)
+      req.on('end', () => {
+        const user = JSON.parse(jsonString)
+        delete user.password
+        user.id = Date.now()
+        users.push(user)
+        console.log(users)
+        res.end(JSON.stringify(user))
+      })
+      return
+    }
+  }
+
+  const data = await fs.readFile('./views/404.html', 'utf8')
+  res.end(data)
+}
+
+const server = http.createServer(requestListener)
+
+server.listen(PORT)
